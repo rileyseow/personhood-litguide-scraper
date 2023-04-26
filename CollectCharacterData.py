23d679d68a551ce1c326a2f65803359c2fdf1ff0
Riley Seow <rielsical@gmail.com>
2023-04-25 21:22:44 -0700

    Rewrote CollectCharacterData.py with asynchronous HTTP request libraries asyncio and aiohttp. This reduced cumulative runtime from a projected 80 minutes (based on subset test run) to an average 6-7 minutes. Full dataset is stored in shmoop_character_data.py.

--- CollectCharacterData.py
+++ CollectCharacterData.py
@@ -3,10 +3,12 @@ File: Scrapes selected character data from all novels on the Shmoop Literature S
       Creates and records data to shmoop_character_data.csv.
       Should be run after a complete and successful run of CollectNovelData.py
 
-Global TODOs: Run on entire dataset.
+Global TODOs: Improve efficiency (see CollectNovelData.py header comment)
 """
 
-import requests
+import asyncio
+import aiohttp
+import time
 from bs4 import BeautifulSoup, SoupStrainer
 import csv
 import re
@@ -16,17 +18,17 @@ import charset_normalizer
 """
 Function: readCSV
 Description: Reads in novel data from shmoop_novel_data.csv
-Parameters: CSV reader object, int columns to read
+Parameters: CSV reader object, int start column, int columns to read
 Return: List of Lists for each row of data read from the CSV
 """
-def readCSV(reader, colsToRead):
+def readCSV(reader, startCol, colsToRead):
     # Exclude header row
     next(reader)
 
     slice = []
     for row in reader:
         colsSubset = []
-        for i in range(colsToRead):
+        for i in range(startCol, startCol + colsToRead):
             colsSubset.append(row[i])
         slice.append(colsSubset)
     return slice
@@ -46,9 +48,26 @@ def writeCSV(writer, row):
     return 0
 
 
-def main():
-    # Initialize requests Session Object for all get requests
-    requests_session = requests.Session()
+"""
+Function: downloadLink
+Description: Uses asyncio / aiohttp library to download a url passed as an argument
+Parameters: aiohttp.ClientSession() object, string (url to download)
+Return: HTML text of one URL page
+"""
+async def downloadLink(session, url):
+    async with session.get(url) as resp:
+        html = await resp.text()
+        return html
+
+
+async def main():
+
+    # Initialize values
+    start_time = time.time()
+    print(f"[{round(time.time() - start_time, 4)}s] (+0s) Starting...")
+    novelID = 0
+    dataToWrite = []
+    characterURLs = []
 
     # Open CSV file with headers to write to
     f = open("shmoop_character_data.csv", "w")
@@ -56,62 +75,95 @@ def main():
     header = ["Novel ID", "Character Name", "Character URL", "Character Description"]
     writeCSV(writer, header)
 
-    print("Initializing data read-in...")
+    print(f"[{round(time.time() - start_time, 4)}s] (+0s) Initializing data read-in...")
 
-    # Read in a list of [Novel ID, Novel URL]s from shmoop_novel_data.csv
+    # Read in a list of Novel URLs from shmoop_novel_data.csv
     f = open("shmoop_novel_data.csv", "r")
     reader = csv.reader(f)
-    CSVNovelData = readCSV(reader, 2)
+    CSVNovelURLs = readCSV(reader, 1, 1)
+    # Flatten list since readCSV will return a list of lists but we only read in one column
+    CSVNovelURLs = [url for subList in CSVNovelURLs for url in subList]
 
-    print("Initializing scrape...")
+    print(f"[{round(time.time() - start_time, 4)}s] (+30-40s) Initializing scrape of all novel homepages...")
 
-    # For each novel...
-    for novel in CSVNovelData[:3]:
+    # Use asyncio to download all novel pages
+    # Note: Can toggle limit of aiohttp simultaneous connections between 10-20 (default 100).
+    # If blocked from server, try reducing limit.)
+    my_conn = aiohttp.TCPConnector(limit=20, ssl=False)
+    # Increase the timeout from default 300s to 600s.
+    my_timeout = aiohttp.ClientTimeout(total=600)
+    async with aiohttp.ClientSession(connector=my_conn, timeout=my_timeout) as session:
+        tasks = [asyncio.ensure_future(downloadLink(session, url)) for url in CSVNovelURLs]
+        novelHTMLs = await asyncio.gather(*tasks, return_exceptions=True)
+
+    print(f"[{round(time.time() - start_time, 4)}s] (+10s) Logging Character Name and Character URL data...")
+
+    for page in novelHTMLs:
         # Generate BeautifulSoup object to parse the novel page for Character Name and Character URL info
         # Use SoupStrainer to limit parsing to the LH nav bar
-        response = requests_session.get(novel[1])
-        html = response.text
-        soup = BeautifulSoup(html, "lxml", parse_only=SoupStrainer("div", class_="nav-menu"))
+        soup = BeautifulSoup(page, "lxml", parse_only=SoupStrainer("div", class_="nav-menu"))
 
         # Find all character tag elements
         try:
             parent = soup.find("a", href=re.compile("characters$")).find_next()
             characterElems = parent.findAll("li", class_=None)
         except:
-            characterElems = "EXC CODE 1"
+            characterElems = ["EXC CODE 1"]
 
-        # Print status data to console
-        currNovel = CSVNovelData.index(novel)
-        if currNovel % 5 == 0: print("Novel ID ", str(currNovel))
-
-        # For each character...
+        # Find name and URL info for each character
         for character in characterElems:
-            # Find name and URL info
-            name = character.text.strip()
-            url = character.find("a").get("href")
-            if url[0] == '/': url = "https://www.shmoop.com" + url
+            if character == "EXC CODE 1":
+                name = "EXC CODE 1"
+                url = "EXC CODE 1"
+            else:
+                # Edge case: "The Princess Bride" incorrectly lists William Goldman (the author) as a character.
+                # Clicking his name leads to a dead ERR_TOO_MANY_REDIRECTS page and crashes this code.
+                # Thus, do not identify him as a character.
+                if character.find("a").get("href").endswith("william-goldman"): continue
+
+                name = character.text.strip()
+                url = character.find("a").get("href")
+                if url[0] == '/': url = "https://www.shmoop.com" + url
 
             # Store the first three values in a list: Novel ID, Character Name, and Character URL
-            ch = [novel[0], name, url]
+            dataToWrite.append([novelID, name, url])
+            characterURLs.append(url)
 
-            # Generate BeautifulSoup object to parse the character's page for Character Description info
-            response = requests_session.get(url)
-            html = response.text
-            soup = BeautifulSoup(html, "lxml", parse_only=SoupStrainer("div", class_="content-wrapper"))
+        novelID += 1
 
-            # Find description info (raw HTML, no cleanup) and append to row data
-            try:
-                desc = soup.findChild().findChild()
-            except:
-                desc = "EXC CODE 2"
-            ch.append(desc)
+    print(f"[{round(time.time() - start_time, 4)}s] (+4-5mins) Initializing scrape of all character pages...")
 
-            # Write entire row of character data to shmoop_character_data.csv
-            writeCSV(writer, ch)
+    # Use asyncio to download all character pages
+    my_conn2 = aiohttp.TCPConnector(limit=20, ssl=False)
+    async with aiohttp.ClientSession(connector=my_conn2, timeout=my_timeout) as session2:
+        tasks2 = [asyncio.ensure_future(downloadLink(session2, url)) for url in characterURLs]
+        characterHTMLs = await asyncio.gather(*tasks2, return_exceptions=True)
+
+    print(f"[{round(time.time() - start_time,4)}s] (+1-2mins) Logging character description data...")
+
+    for character, html in zip(dataToWrite, characterHTMLs):
+        # Generate BeautifulSoup object to parse the character's page for Character Description info
+        soup = BeautifulSoup(html, "lxml", parse_only=SoupStrainer("div", class_="content-wrapper"))
+
+        # Find description info (raw HTML, no cleanup) and append to row data
+        try:
+            desc = soup.findChild().findChild()
+        except:
+            desc = "EXC CODE 2"
+
+        character.append(desc)
+
+    print(f"[{round(time.time() - start_time, 4)}s] (+2s) Initializing data read-out...")
+
+    # Write out all rows of character data to shmoop_character_data.csv
+    for row in dataToWrite:
+        writeCSV(writer, row)
 
     # Close the CSV file
     f.close()
 
+    print(f"[{round(time.time() - start_time, 4)}s] (+0s) Finished.")
+
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
